4a82913197a7b3b4b341fec773da68212b9e789c
Vladislav <vasch_ukr@ukr.net>
2021-11-14 13:30:20 +0200

    simple card

--- src/components/SimpleMovieCard/SimpleMovieCard.tsx
+++ src/components/SimpleMovieCard/SimpleMovieCard.tsx
@@ -1,9 +1,61 @@
 import React from "react";
-import { Card } from "@mui/material";
+import { Card, Box, CardContent, Typography } from "@mui/material";
+import { IMovie } from "../../modules";
+import TheatersIcon from '@mui/icons-material/Theaters';
+import format from "date-fns/fp/format";
+import { useHistory } from "react-router";
+import './SimpleMovieCard.scss';
 
-const SimpleMovieCard:React.FC = () => {
+interface Props {
+  item: IMovie 
+}
+
+const SimpleMovieCard:React.FC<Props> = ({ item }) => {
+  const history = useHistory();
+  const formatDate = ():string => {
+    const dateArr = item.release_date.split('-');
+    return format('PPP', new Date(+dateArr[0], +dateArr[1], +dateArr[2]));
+  }
+
+  const goToMovie = ():void => {
+    history.push(`/movie/${item.id}`)
+  }
+
   return(
-    <Card></Card>
+    <Card
+      className='simple-card'
+      sx={{ cursor: 'pointer', borderRadius: '15px', display: 'flex' }}
+      onClick={goToMovie}
+    >
+      <Box
+        sx={{
+          width: '94px',
+          height: '160px',
+          minWidth: '94px',
+          backgroundColor: '#c4c4c4',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+          }}
+      >
+        {
+          !!item.poster_path ?
+          <img src={`${process.env.REACT_APP_IMG}${item.poster_path}`} alt={item.title} /> :
+          <TheatersIcon fontSize='large' />
+        }
+      </Box>
+      <CardContent sx={{ flexGrow: 1, textAlign: 'start' }}>
+        <Typography component='p' variant='h5' sx={{ fontWeight: 600 }}>
+          { item.title }
+        </Typography>
+        <Typography sx={{ color: '#c4c4c4' }}>
+          { formatDate() }
+        </Typography>
+        <Typography sx={{ display: '-webkit-box', WebkitLineClamp: 2, WebkitBoxOrient: 'vertical', textOverflow: 'ellipsis', overflow: 'hidden' }} mt={2}>
+          { item.overview }
+        </Typography>
+      </CardContent>
+    </Card>
   )
 }
 
--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useLocation } from "react-router";
 import { useFetchSearchMoviesQuery } from "../../services/MoviesService";
 import SimpleMovieCard from "../../components/SimpleMovieCard/SimpleMovieCard";
+import { Container, Grid } from "@mui/material";
 import './SearchPage.scss';
 
 const SearchPage:React.FC = () => {
@@ -11,7 +12,22 @@ const SearchPage:React.FC = () => {
   const resuts = useFetchSearchMoviesQuery(searchValue);
 
   return(
-    <div className="search-page"></div>
+    <div className="search-page">
+      <Container maxWidth='xl'>
+        <Grid container spacing={2}>
+          {
+            resuts.data?.results && 
+            resuts.data.results.map(movie => {
+              return(
+                <Grid item xs={6} key={movie.id}>
+                  <SimpleMovieCard item={movie} />
+                </Grid>
+              )
+            })
+          }
+        </Grid>
+      </Container>
+    </div>
   )
 }
 
